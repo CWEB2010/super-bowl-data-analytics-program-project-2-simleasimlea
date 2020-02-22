a4fdc1edcce51f6514d421d63192eff1911dd27d
Simonson <simlea@dunwoody.edu>
2020-02-21 22:16:03 -0600

    getting started on project 2

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,18 +1,82 @@
 ﻿using System;
+using System.IO;
 
 namespace Project_Two
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //getting started
-			
-			/**Your application should allow the end user to pass end a file path for output 
-            * or guide them through generating the file.
-            **/
+	class Program
+	{
+		static readonly string textFile = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Project.csv";
+		static string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
 
-           
-        }
-    }
-}
+		static void Main(string[] args)
+		{
+
+			if (File.Exists(textFile))
+			{
+				using (StreamReader file = new StreamReader(textFile))
+				{
+					int counter = 0;
+					string ln;
+
+					while ((ln = file.ReadLine()) != null)
+					{
+
+						Console.WriteLine(ln);
+						counter++;
+					}
+					file.Close();
+					Console.WriteLine($"File has {counter} lines.");
+				}
+			}
+
+		
+			using (StreamWriter file = new StreamWriter(path))
+			{
+				string[] lines = File.ReadAllLines(textFile); ;
+				foreach (string line in lines)
+
+				{
+					string[] col = line.Split(',');
+
+					WinningTeam team = new WinningTeam(date: col[0], sB: col[1], qBWinner: col[2], coachWinner: col[3], winner: col[4], winningPts: col[5],
+					 qBLoser: col[6], coachLoser: col[7], loser: col[8], losingPts: col[9], mVP: col[10], stadium: col[11], city: col[12], state: col[13]);
+
+					WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
+
+					file.WriteLine(line);
+
+				}
+			
+
+
+				//using (StreamWriter bfile = new StreamWriter(path))
+				//{
+				//	string[] blines = File.ReadAllLines(textFile); ;
+				//	foreach (string line in blines)
+
+				//	{
+				//		string[] col = line.Split(',');
+
+
+				//		WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
+
+
+				//		file.WriteLine("testing" + line);
+				//		//file.Close();
+				//	}
+
+
+
+
+
+				}
+
+				Console.ReadKey();
+			}
+		}
+	}
+
+
+
+
+
---
+++ Project_Two/WinningTeam.cs
@@ -0,0 +1,51 @@
+﻿namespace Project_Two
+{
+	internal class WinningTeam
+	{
+		private object date;
+		private object sB;
+		private object qBWinner;
+		private object coachWinner;
+		private object winner;
+		private object winningPts;
+		private object qBLoser;
+		private object coachLoser;
+		private object loser;
+		private object losingPts;
+		private object mVP;
+		private object stadium;
+		private object city;
+		private object state;
+
+		public WinningTeam(string v)
+		{
+		}
+
+		public WinningTeam(string winner, string sB, string date, string qBWinner, string mVP)
+		{
+			this.winner = winner;
+			this.sB = sB;
+			this.date = date;
+			this.qBWinner = qBWinner;
+			this.mVP = mVP;
+		}
+
+		public WinningTeam(object date, object sB, object qBWinner, object coachWinner, object winner, object winningPts, object qBLoser, object coachLoser, object loser, object losingPts, object mVP, object stadium, object city, object state)
+		{
+			this.date = date;
+			this.sB = sB;
+			this.qBWinner = qBWinner;
+			this.coachWinner = coachWinner;
+			this.winner = winner;
+			this.winningPts = winningPts;
+			this.qBLoser = qBLoser;
+			this.coachLoser = coachLoser;
+			this.loser = loser;
+			this.losingPts = losingPts;
+			this.mVP = mVP;
+			this.stadium = stadium;
+			this.city = city;
+			this.state = state;
+		}
+	}
+}
