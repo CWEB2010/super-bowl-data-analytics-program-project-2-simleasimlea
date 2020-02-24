37bcddbf8ec6e17699b7fbc99dad959ffbf135df
Simonson <simlea@dunwoody.edu>
2020-02-23 22:12:36 -0600

    added try catch plus more

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Project_Two
@@ -7,10 +8,12 @@ namespace Project_Two
 	{
 		static readonly string textFile = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Project.csv";
 		static string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
+		
 
 		static void Main(string[] args)
 		{
-
+			try
+			{
 			if (File.Exists(textFile))
 			{
 				using (StreamReader file = new StreamReader(textFile))
@@ -18,63 +21,77 @@ namespace Project_Two
 					int counter = 0;
 					string ln;
 
+					List<WinningTeam> Team = new List<WinningTeam>();
+
+					
 					while ((ln = file.ReadLine()) != null)
 					{
 
-						Console.WriteLine(ln);
-						counter++;
+							string[] col = ln.Split(',');
+
+							{
+				
+							WinningTeam team = new WinningTeam(date: col[0], sB: col[1], qBWinner: col[2], coachWinner: col[3], winner: col[4], winningPts: col[5],
+							qBLoser: col[6], coachLoser: col[7], loser: col[8], losingPts: col[9], mVP: col[10], stadium: col[11], city: col[12], state: col[13]);
+							Team.Add(team);
+							Console.WriteLine(col[7]);
+							counter++;
+						}
 					}
 					file.Close();
 					Console.WriteLine($"File has {counter} lines.");
 				}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("The process failed: {0}", e.ToString());
 			}
 
-		
-			using (StreamWriter file = new StreamWriter(path))
-			{
-				string[] lines = File.ReadAllLines(textFile); ;
-				foreach (string line in lines)
+			//using (StreamWriter file = new StreamWriter(path))
+			//{
+			//	string[] lines = File.ReadAllLines(textFile);
+			//	foreach (string line in lines)
 
-				{
-					string[] col = line.Split(',');
+			//	{
+			//		string[] col = line.Split(',');
 
-					WinningTeam team = new WinningTeam(date: col[0], sB: col[1], qBWinner: col[2], coachWinner: col[3], winner: col[4], winningPts: col[5],
-					 qBLoser: col[6], coachLoser: col[7], loser: col[8], losingPts: col[9], mVP: col[10], stadium: col[11], city: col[12], state: col[13]);
+			//		WinningTeam team = new WinningTeam(date: col[0], sB: col[1], qBWinner: col[2], coachWinner: col[3], winner: col[4], winningPts: col[5],
+			//		 qBLoser: col[6], coachLoser: col[7], loser: col[8], losingPts: col[9], mVP: col[10], stadium: col[11], city: col[12], state: col[13]);
 
-					WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
+			//		WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
 
-					file.WriteLine(line);
+			//		file.WriteLine(line);
+			//	}
 
-				}
-			
-
-
-				//using (StreamWriter bfile = new StreamWriter(path))
-				//{
-				//	string[] blines = File.ReadAllLines(textFile); ;
-				//	foreach (string line in blines)
-
-				//	{
-				//		string[] col = line.Split(',');
+			//	//file.Close();
 
 
-				//		WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
+			//	using (StreamWriter bfile = new StreamWriter(path))
+			//	{
+			//		string[] blines = File.ReadAllLines(textFile);
+			//		foreach (string bline in blines)
+
+			//		{
+			//			string[] col = bline.Split(',');
 
 
-				//		file.WriteLine("testing" + line);
-				//		//file.Close();
-				//	}
+			//			WinningTeam team2 = new WinningTeam(winner: col[4], sB: col[1], date: col[0], qBWinner: col[2], mVP: col[10]);
 
 
+			//			file.WriteLine("testing" + bline);
+			//		}
 
+			//		bfile.Close();
+			//	}
 
-
-				}
 
 				Console.ReadKey();
 			}
 		}
 	}
+
+
 
 
 
