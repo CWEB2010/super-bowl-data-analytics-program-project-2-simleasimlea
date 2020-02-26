d56833629e557a8b4267612bf14c58347e663246
Simonson <simlea@dunwoody.edu>
2020-02-26 09:51:46 -0600

    foundation laid

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,43 +1,47 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace Project_Two
 {
 	class Program
 	{
 		static readonly string textFile = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Project.csv";
-		static string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
-
+		static readonly string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
 
 		static void Main(string[] args)
 		{
+			List<WinningTeam> Team = new List<WinningTeam>();
+
 			try
 			{
 				if (File.Exists(textFile))
 				{
 					using (StreamReader file = new StreamReader(textFile))
-						
+
 					{
 						int counter = 0;
 						string ln;
-
-						List<WinningTeam> Team = new List<WinningTeam>();
-
+						Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
 
 						while ((ln = file.ReadLine()) != null)
+
 						{
 
-							string[] col = ln.Split(',');
+							string[] col = CSVParser.Split(ln);
 
 							{
-								if (counter > 1)
+								//added this because I was going to skip the header, but for some reason I don't have to...figure out why it is skipping
+								if (counter > 0)
 
 								{
 									WinningTeam team = new WinningTeam(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]);
 									Team.Add(team);
-		
+									Console.WriteLine(ln);
+
 								}
 								counter++;
 							}
@@ -50,57 +54,61 @@ namespace Project_Two
 				}
 			}
 			catch (Exception e)
-			//catch FileNotFoundException
+			//maybe later add specific exception such as catch FileNotFoundException
 			{
 				Console.WriteLine("The process failed: {0}", e.ToString());
 			}
 
+
 			using (StreamWriter file = File.CreateText(path))
-			
 
-			//using (StreamWriter file = new StreamWriter(path))
 			{
-				string[] lines = File.ReadAllLines(textFile);
-				foreach (string line in lines)
 
-				{
-					string[] col = line.Split(',');
+			file.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
+			Console.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
 
-					WinningTeam team = new WinningTeam(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]);
 
-					//file.WriteLine(line);
-				}
+			foreach (WinningTeam item in Team)
+			{
 
-				file.Close();
+				file.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
+				Console.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
 
+			}
+
+			file.Close();
 
 
 				using (StreamWriter bfile = File.AppendText(path))
-				//using (StreamWriter bfile = new StreamWriter(path))
+				//Generate a list of the top five attended super bowls
+
 				{
-					string[] blines = File.ReadAllLines(textFile);
 
-					Console.WriteLine("Winner, Sdate, QBWinner, CoachWinner, MVP, WinningPts,LosingPts");
+					bfile.WriteLine("\n\nCreate the right headings, LeAnn");
+					Console.WriteLine("\n\nCreate the right headings, LeAnn");
 
-					foreach (string bline in blines)
+					int counter = 0;
 
+					foreach (WinningTeam item in Team.OrderByDescending(x => x.Attendance))
 					{
-						string[] col = bline.Split(',');
+						if (counter < 5)
 
-						WinningTeam team = new WinningTeam(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]);
-						
-						bfile.WriteLine("testing" + team.Winner + string.Format("{0:y yy yyy yyyy}", team.Sdate) + team.QBWinner + team.CoachWinner + team.MVP + team.WinningPts + team.LosingPts);
-
+						{
+							bfile.WriteLine(item.Sdate.Year + "\t" + item.Winner + "\t" + item.Loser + "\t" + item.City + "\t" + item.State + "\t" + item.Stadium);
+						}
+						counter++;
 					}
 
-					bfile.Close();
+
+
+
 				}
 
-
-				Console.ReadKey();
-			}
+			file.Close();
+			Console.ReadKey();
 		}
 	}
+}
 }
 
 
--- Project_Two/WinningTeam.cs
+++ Project_Two/WinningTeam.cs
@@ -6,17 +6,17 @@ public class WinningTeam
 {
 	//Declarations
 
-	public string Sdate { get; set; }
+	public DateTime Sdate { get; set; }
 	public string SB { get; set; }
-	public string Attendance { get; set; }
+	public int Attendance { get; set; }
 	public string QBWinner { get; set; }
 	public string CoachWinner { get; set; }
 	public string Winner { get; set; }
-	public string WinningPts { get; set; }
+	public int WinningPts { get; set; }
 	public string QBLoser { get; set; }
 	public string CoachLoser { get; set; }
 	public string Loser { get; set; }
-	public string LosingPts { get; set; }
+	public int LosingPts { get; set; }
 	public string MVP { get; set; }
 	public string Stadium { get; set; }
 	public string City { get; set; }
@@ -25,17 +25,17 @@ public class WinningTeam
 
 	public WinningTeam(string Sdate, string SB, string Attendance, string QBWinner, string CoachWinner, string Winner, string WinningPts, string QBLoser, string CoachLoser, string Loser, string LosingPts, string MVP, string Stadium, string City, string State)
 	{
-		this.Sdate = Sdate;
+		this.Sdate = DateTime.Parse(s: Sdate);
 		this.SB = SB;
-		this.Attendance = Attendance;
+		this.Attendance = int.Parse(Attendance);
 		this.QBWinner = QBWinner;
 		this.CoachWinner = CoachWinner;
 		this.Winner = Winner;
-		this.WinningPts = WinningPts;
+		this.WinningPts = int.Parse(WinningPts);
 		this.QBLoser = QBLoser;
 		this.CoachLoser = CoachLoser;
 		this.Loser = Loser;
-		this.LosingPts = LosingPts;
+		this.LosingPts = int.Parse(LosingPts);
 		this.MVP = MVP;
 		this.Stadium = Stadium;
 		this.City = City;
