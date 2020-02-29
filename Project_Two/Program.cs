b83017f9a0281424d29aa3dff63ff189e7735b6e
Simonson <simlea@dunwoody.edu>
2020-02-29 15:51:12 -0600

    user input of paths

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -9,106 +9,136 @@ namespace Project_Two
 {
 	class Program
 	{
-		static readonly string textFile = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Project.csv";
-		static readonly string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
+		//static readonly string textFile = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Project.csv";
+		//static readonly string path = @"C:\Users\simlea\Desktop\MyAssignments\Project2\Super_Bowl_Report.txt";
+		
 
 		static void Main(string[] args)
 		{
 			List<WinningTeam> Team = new List<WinningTeam>();
+			string primingValue = "";
+			string textFile = "";
+			string path = "";
 
-			try
+			Console.WriteLine("\nWhat is your name? (Enter X to exit.)");
+
+			primingValue = Convert.ToString(Console.ReadLine().ToUpper());//using primingValue for user name and also to exit out
+
+			if (primingValue != "X")
 			{
-				if (File.Exists(textFile))
+				Console.WriteLine("\nWhat is the path (folders & file name)\n" +
+				"for the file from which we should read?\n\n" +
+				"for example: C:\\Users\\simlea\\Desktop\\MyAssignments\\Project2\\Super_Bowl_Project.csv");
+
+				textFile = Console.ReadLine();
+				textFile = @"textFile";
+
+				Console.WriteLine(textFile);
+
+				Console.WriteLine("\nWhat is the path (folders & file name)\n" +
+				"for the file to which we should write?\n\n" +
+				"for example: C:\\Users\\simlea\\Desktop\\MyAssignments\\Project2\\Super_Bowl_Report.txt");
+
+				path = Console.ReadLine();
+				path = @"path";
+
+				Console.WriteLine(path);
+
+				try
 				{
-					using (StreamReader file = new StreamReader(textFile))
-
+					if (File.Exists(textFile))
 					{
-						int counter = 0;
-						string ln;
-						Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
-
-						while ((ln = file.ReadLine()) != null)
+						using (StreamReader file = new StreamReader(textFile))
 
 						{
+							int counter = 0;
+							string ln;
+							Regex CSVParser = new Regex(",(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))");
 
-							string[] col = CSVParser.Split(ln);
+							while ((ln = file.ReadLine()) != null)
 
 							{
-								//added this because I was going to skip the header, but for some reason I don't have to...figure out why it is skipping
-								if (counter > 0)
+
+								string[] col = CSVParser.Split(ln);
 
 								{
-									WinningTeam team = new WinningTeam(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]);
-									Team.Add(team);
-									Console.WriteLine(ln);
+									//added this because I was going to skip the header, but for some reason I don't have to...figure out why it is skipping
+									if (counter > 0)
 
+									{
+										WinningTeam team = new WinningTeam(col[0], col[1], col[2], col[3], col[4], col[5], col[6], col[7], col[8], col[9], col[10], col[11], col[12], col[13], col[14]);
+										Team.Add(team);
+										Console.WriteLine(ln);
+
+									}
+									counter++;
 								}
-								counter++;
 							}
+							file.Close();
+							Console.WriteLine($"File has {counter} lines.");
+
+
 						}
-						file.Close();
-						Console.WriteLine($"File has {counter} lines.");
-
-
 					}
 				}
-			}
-			catch (Exception e)
-			//maybe later add specific exception such as catch FileNotFoundException
-			{
-				Console.WriteLine("The process failed: {0}", e.ToString());
-			}
+				catch (Exception e)
+				//maybe later add specific exception such as catch FileNotFoundException
+				{
+					Console.WriteLine("The process failed: {0}", e.ToString());
+				}
 
-
-			using (StreamWriter file = File.CreateText(path))
-
-			{
-
-			file.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
-			Console.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
-
-
-			foreach (WinningTeam item in Team)
-			{
-
-				file.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
-				Console.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
-
-			}
-
-			file.Close();
-
-
-				using (StreamWriter bfile = File.AppendText(path))
-				//Generate a list of the top five attended super bowls
+				using (StreamWriter file = File.CreateText(path))
 
 				{
 
-					bfile.WriteLine("\n\nCreate the right headings, LeAnn");
-					Console.WriteLine("\n\nCreate the right headings, LeAnn");
+					file.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
+					Console.WriteLine("Team Name" + "\t" + "Year of Win" + "\t" + "Winning QB" + "\t" + "Winning Coach" + "\t" + "MVP" + "\t" + "Point Difference");
 
-					int counter = 0;
 
-					foreach (WinningTeam item in Team.OrderByDescending(x => x.Attendance))
+					foreach (WinningTeam item in Team)
 					{
-						if (counter < 5)
 
-						{
-							bfile.WriteLine(item.Sdate.Year + "\t" + item.Winner + "\t" + item.Loser + "\t" + item.City + "\t" + item.State + "\t" + item.Stadium);
-						}
-						counter++;
+						file.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
+						Console.WriteLine(item.Winner + "\t" + item.Sdate.Year + "\t" + item.QBWinner + "\t" + item.CoachWinner + "\t" + item.MVP + "\t" + (item.WinningPts - item.LosingPts));
+
 					}
 
+					file.Close();
 
+
+					using (StreamWriter bfile = File.AppendText(path))
+					//Generate a list of the top five attended super bowls
+
+					{
+
+						bfile.WriteLine("\n\nCreate the right headings, LeAnn");
+						Console.WriteLine("\n\nCreate the right headings, LeAnn");
+
+						int counter = 0;
+
+						foreach (WinningTeam item in Team.OrderByDescending(x => x.Attendance))
+						{
+							if (counter < 5)
+
+							{
+								bfile.WriteLine(item.Sdate.Year + "\t" + item.Winner + "\t" + item.Loser + "\t" + item.City + "\t" + item.State + "\t" + item.Stadium);
+							}
+							counter++;
+						}
+
+
+
+
+					}
+
+					file.Close();
+					primingValue = Convert.ToString(Console.ReadLine().ToUpper());
 
 
 				}
-
-			file.Close();
-			Console.ReadKey();
+			}
 		}
 	}
-}
 }
 
 
